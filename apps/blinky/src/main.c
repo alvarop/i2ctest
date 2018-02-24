01dcc4200d81721651febf29fd5439f10c8ab187
alvaro <gitkraken@alvaroprieto.com>
2018-02-24 05:15:16 -0800

    I2C Bug Example Update
    Test runs on the nrf52dk (PCA10040)
    To test i2c bug, load on the nrf52 (newt run nrf52dk 0)
    Measure pin p0.27 (SCL) with a multimeter, it should be pulled high
    then connect over serial terminal and run the command 'i2c_scan 0'
    Measure p0.27 again and it will be low, which it shouldn't
    Note that there are no devices connected that could be pulling SCL low
    Now run the command `'2c_fix' and measure p0.27 again
    It should be high

--- apps/blinky/src/main.c
+++ apps/blinky/src/main.c
@@ -26,18 +26,61 @@
 #include "hal/hal_gpio.h"
 #include "hal/hal_i2c.h"
 #include "mcu/nrf52_hal.h"
+#include <shell/shell.h>
+#include <console/console.h>
+#include <nrf.h>
 
-/* For LED toggling */
-int g_led_pin;
+#define BLINK_TASK_PRI         (10)
+#define BLINK_STACK_SIZE       (256)
+struct os_task blink_task;
+os_stack_t blink_task_stack[BLINK_STACK_SIZE];
 
-/**
- * main
- *
- * The main task for the project. This function initializes packages,
- * and then blinks the BSP LED in a loop.
- *
- * @return int NOTE: this function should never return!
- */
+static int i2c_fix_cli_cmd(int argc, char **argv);
+
+static struct shell_cmd i2c_fix_cmd_struct = {
+    .sc_cmd = "i2c_fix",
+    .sc_cmd_func = i2c_fix_cli_cmd
+};
+
+#define NRF52_SCL_PIN_CONF                                              \
+    ((GPIO_PIN_CNF_SENSE_Disabled << GPIO_PIN_CNF_SENSE_Pos) |          \
+      (GPIO_PIN_CNF_DRIVE_S0D1    << GPIO_PIN_CNF_DRIVE_Pos) |          \
+      (GPIO_PIN_CNF_PULL_Pullup   << GPIO_PIN_CNF_PULL_Pos) |           \
+      (GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos) |          \
+      (GPIO_PIN_CNF_DIR_Input     << GPIO_PIN_CNF_DIR_Pos))
+
+
+static int
+i2c_fix_cli_cmd(int argc, char **argv) {
+    console_printf("Trying to recover\n");
+
+    NRF_TWIM0->ENABLE = TWI_ENABLE_ENABLE_Disabled;
+    NRF_TWIM0->ENABLE = TWI_ENABLE_ENABLE_Enabled;
+    return 0;
+}
+
+void blink_task_func(void *arg) {
+
+    console_printf("I2C Test!\n");
+
+    hal_gpio_init_out(LED_BLINK_PIN, 1);
+
+    while (1) {
+        os_time_delay(OS_TICKS_PER_SEC);
+        hal_gpio_toggle(LED_BLINK_PIN);
+    }
+}
+
+//
+// Test runs on the nrf52dk (PCA10040)
+// To test i2c bug, load on the nrf52 (newt run nrf52dk)
+// Measure pin p0.27 (SCL) with a multimeter, it should be pulled high
+// then connect over serial terminal and run the command 'i2c_scan 0'
+// Measure p0.27 again and it will be low, which it shouldn't
+// Note that there are no devices connected that could be pulling SCL low
+// Now run the command `'2c_fix' and measure p0.27 again
+// It should be high
+
 int
 main(int argc, char **argv)
 {
@@ -45,47 +88,21 @@ main(int argc, char **argv)
 
     sysinit();
 
-    g_led_pin = LED_BLINK_PIN;
-    hal_gpio_init_out(g_led_pin, 1);
+    os_task_init(
+        &blink_task,
+        "blink_task",
+        blink_task_func,
+        NULL,
+        BLINK_TASK_PRI,
+        OS_WAIT_FOREVER,
+        blink_task_stack,
+        BLINK_STACK_SIZE);
 
-    static const struct nrf52_hal_i2c_cfg hal_i2c_cfg = {
-        .scl_pin = 27,
-        .sda_pin = 26,
-        .i2c_frequency = 100    /* 100 kHz */
-    };
-
-    rc = hal_i2c_init(0, (void *)&hal_i2c_cfg);
-    assert(rc == 0);
+    shell_cmd_register(&i2c_fix_cmd_struct);
 
     while (1) {
-
-        // Attempt dummy i2c write
-        uint8_t wbuff[1] = {0};
-        struct hal_i2c_master_data pdata = {
-            .address = 0x44,
-            .len = 1,
-            .buffer = wbuff
-        };
-        hal_i2c_master_write(0, &pdata, 100, 1);
-
-        // Write will fail and SCL will get stuck low
-
-        /* Wait one second */
-        os_time_delay(OS_TICKS_PER_SEC);
-
-
-        // Attempt to recover SCL (doesn't work for me)
-        rc = hal_i2c_init(0, (void *)&hal_i2c_cfg);
-        assert(rc == 0);
-
-        /* Wait one second */
-        os_time_delay(OS_TICKS_PER_SEC * 2);
-
-        /* Toggle the LED */
-        hal_gpio_toggle(g_led_pin);
+        os_eventq_run(os_eventq_dflt_get());
     }
     assert(0);
-
     return rc;
 }
-
